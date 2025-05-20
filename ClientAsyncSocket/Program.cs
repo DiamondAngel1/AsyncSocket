8797853ea9a3b7a95788c6b70cdabaefbe757add
DiamondAngel1 <tararukolga78@gmail.com>
2025-05-20 16:27:16 +0300

    Correct

--- ClientAsyncSocket/Program.cs
+++ ClientAsyncSocket/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+var endPoint = new IPEndPoint(IPAddress.Parse("91.238.103.107"), 8888);
+Console.WriteLine($"Search server {endPoint}");
+await clientSocket.ConnectAsync(endPoint);
+
+string text = Console.ReadLine();
+byte[] data = Encoding.UTF8.GetBytes(text);
+await clientSocket.SendAsync(data);
+byte[] buffer = new byte[10024];
+int bytes = await clientSocket.ReceiveAsync(buffer);
+string textServerResp = Encoding.UTF8.GetString(buffer, 0, bytes);
+Console.WriteLine($"Server response: {textServerResp}");
+clientSocket.Shutdown(SocketShutdown.Both);
+clientSocket.Close();
--- ServerAsyncSocket/Program.cs
+++ ServerAsyncSocket/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+var ipEndPoint = new IPEndPoint(IPAddress.Any, 2009);
+var listenear = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+listenear.Bind(ipEndPoint);
+listenear.Listen(20);
+
+Console.WriteLine($"Сервер запущено {ipEndPoint}");
+
+while (true)
+{
+    Socket client = await listenear.AcceptAsync();
+    _ = Task.Run(() => HandleClientAsync(client));
+
+}
+async Task HandleClientAsync(Socket client)
+{
+    Console.WriteLine($"Клієнт {client.RemoteEndPoint} підключився");
+    byte[] buffer = new byte[10024];
+    int bytesRead = await client.ReceiveAsync(buffer);
+    string message = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+    Console.WriteLine("Клієнт прислав повідомлення: " + message);
+    string response = $"Сервер отримав ваше повідомлення: {DateTime.UtcNow.ToLocalTime().ToShortTimeString()} - {client.RemoteEndPoint}";
+    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+    await client.SendAsync(responseBytes);
+    client.Shutdown(SocketShutdown.Both);
+    client.Close();
+}
